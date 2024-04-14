8df4026ed9393ac47c16cdf877e36a5b87426680
Okware-L <lewisokware@gmail.com>
2024-04-14 22:06:54 +0300

    doc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <ClerkProvider>
+      <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/jmlogoblack.svg",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
         <body className={inter.className}>
           <link rel="icon" href="/jmlogoblack.svg" sizes="any" />
           {children}
